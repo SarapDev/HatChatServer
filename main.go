732e091fbe640650605eb1410ef276b2c022aa34
Mikhail Sarapulov <sarapulov@kolesa.kz>
2023-10-04 00:50:17 +0600

    refactor: remove old html, add license

--- main.go
+++ main.go
@@ -10,25 +10,11 @@ import (
 
 var addr = flag.String("addr", ":8080", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request)  {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
-}
-
-func main()  {
+func main() {
 	flag.Parse()
 	room := chat.NewRoom()
 	go room.RunRoom()
 
-	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		server := server2.NewWsServer(w, r, room)
 		server.Serve()
